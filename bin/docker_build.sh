bc75cb033934c8f77dace323d074aed9692175eb
Jeroen Knoops <jeroen.knoops@gmail.com>
2018-09-19 14:36:18 +0200

    Adds travis-ci to test building the docker images

---
+++ bin/docker_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "You need to provide a directory with a Dockerfile in it."
+  exit 1
+fi
+
+builddir=$1
+currentdir=$(pwd)
+
+echo $currentdir
+
+echo "Building docker image: $builddir"
+echo "-------------------------------------------------------------------------"
+cd $builddir
+docker build .
+echo "============================================================================================"
+echo "Finished building docker image: $builddir"
+echo "============================================================================================"
